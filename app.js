458b7db892d67ede1869d8c1c3ee65963b26e7a6
Lynne Daniels <lynne33477@gmail.com>
2017-01-16 07:17:04 -0500

    wip

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var 
-	redSound = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
-	blueSound = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
-	yellowSound = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
-	greenSound = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
-liteColor = {
+var	redSound = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+var	blueSound = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+var	yellowSound = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+var	greenSound = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+var liteColor = {
 	red : {dark: "#7F1818",
 			lit: "#c22525",
 			sound: redSound},
@@ -18,21 +17,17 @@ liteColor = {
 			sound: greenSound}			
 };
 
-var sequence = [], 
-	count = 3,
-	timetest,
-	userSequence = [],
-	strict = false,
-	gameOn = false;
+var sequence = [];
+var count;// says how many at each stage.  goes up to 20, set this max in the initialize function
+var timetest;
+var userSequence = [];
+var strict = false;
+var gameOn = false;
 
 function lights(sequence){ //input array, allowed vals are 'red', 'blue', 'yellow', 'green' html must use those ids and colors
 	function light(color){
 	
 		var panel = document.getElementById(color);
-			//sound = color + 'Sound';
-			//console.log(sound);
-		//blueSound.play();
-		//console.log(liteColor[color]);
 		liteColor[color].sound.play();
 		panel.style.background = liteColor[color].lit;
 		timetest = setTimeout(function(){panel.style.background = liteColor[color].dark;},800);
@@ -48,87 +43,114 @@ function lights(sequence){ //input array, allowed vals are 'red', 'blue', 'yello
 	}
 }
 
-function makeSequence(num){
+function makeSequence(num){// returns an array of color names with length = num, make sequence of 20 for each new game
 	var color,
 		rnd,
 		sequence = [],
 		colors = (['red', 'blue', 'yellow', 'green']);
 	for (var i = 0; i < num; i++){
 		rnd = Math.floor(Math.random()*4);
-		//console.log(rnd);
 		sequence.push(colors[rnd]);
 	}
+	console.log(sequence);
 	return sequence;
 }
 function initializeGame(){
 	sequence = makeSequence(3);//TODO change to 20 after done testing
-	count = 1;  //TODO change to 20 or one something else later
+	count = 1;  //
 	userSequence = [];
-	//console.log(sequence);
-	//console.log(userSequence);
+
 }
 function playPattern(howMany){
-
+	userSequence = [];
 	var partSequence = sequence.slice(0, howMany);
 	lights(partSequence);
+	console.log('playing Pattern');
 }
-
+ 
 function compareUserSequence(clickedButton){
 	userSequence.push(clickedButton);
+	console.log(userSequence);
+	//console.log(userSequence.join(',') + ' ' + sequence.slice(0,count).join(','));
+	//if ((userSequence[userSequence.length - 1] === (sequence[userSequence.length - 1]))){
+		
+		
+	
 	if (userSequence.join(',') === sequence.slice(0,count).join(',')){
-		console.log('good choice' + playPattern);
+		console.log(userSequence);
+		console.log('good choice ' +  userSequence  + ' vs ' + sequence.slice(0,count));
 		count = count + 1;
-		userSequence = [];
-		setTimeout(function(){
-			playPattern(count);
-		}, 800);
-		//playPattern(count);
-	} else console.log(userSequence.join(',') === sequence.slice(0,count).join(','));
-}
+		//userSequence = [];
+		(function(){
+			setTimeout(function(){
+				playPattern(count);
+			}, 1000);
+		})();
 
+	} else {
+		//console.log('wrong color ' + userSequence + ' vs ' + sequence.slice(0,count));//+ userSequence.join(',') === sequence.slice(0,count).join(',')}
+		if (clickedButton === sequence[userSequence.length - 1]){
+			console.log('okydoky');
+		}else{
+			console.log('wrong button');
+			lights(['red']);
+			lights(['yellow']);
+			lights(['blue']);
+			lights(['green']);
+			(function(){
+				setTimeout(function(){
+					playPattern(count);
+				}, 1000);
+			})();
+		}
+		
+		;
+	
+	};
+}
 
 
 document.getElementById("red").addEventListener("click", function(event){
 	event.preventDefault(); // prevents resize on iphone dblclick
 	lights(['red']);
 	compareUserSequence('red');
-	console.log('____ clicked');
+	//console.log('____ clicked');
 });
 document.getElementById("blue").addEventListener("click", function(event){
 	event.preventDefault(); // prevents resize on iphone dblclick
 	lights(['blue']);
 	compareUserSequence('blue');
-	console.log('____ clicked');
+	//console.log('____ clicked');
 });
 document.getElementById("yellow").addEventListener("click", function(event){
 	event.preventDefault(); // prevents resize on iphone dblclick
 	lights(['yellow']);
 	compareUserSequence('yellow');
-	console.log('____ clicked');
+	//console.log('____ clicked');
 });
 document.getElementById("green").addEventListener("click", function(event){
 	event.preventDefault(); // prevents resize on iphone dblclick
 	lights(['green']);
 	compareUserSequence('green');
-	console.log('____ clicked');
+	//console.log('____ clicked');
 });
 document.getElementById("start").addEventListener("click", function(event){//start
 	event.preventDefault(); // prevents resize on iphone dblclick
 	initializeGame(); //TODO change to 20 when done testing
 	playPattern(count);
-	console.log('start clicked');
+	//console.log('start clicked');
 });		
 document.getElementById("strict").addEventListener("click", function(event){//strict
 	event.preventDefault(); // prevents resize on iphone dblclick
 	strict =! strict;
 	//TODO make light on/off
-	console.log('strict clicked');
+	//console.log('strict clicked');
 });
 document.getElementById("on-off").addEventListener("click", function(event){//on-off
 	event.preventDefault(); // prevents resize on iphone dblclick
 	gameOn =! gameOn;
 	initializeGame();
 	//TODO on-off css toggle left/right float of blue div
-	console.log('on-off clicked');
+	//console.log('on-off clicked');
 });
 		
